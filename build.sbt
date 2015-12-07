d0034bf77dfff1e3ba35cb1f4e60c0c685f6861b
DynamicScope <sungjin63@gmail.com>
2015-12-07 14:27:48 +0900

    Data migration script with configuration file.

--- build.sbt
+++ build.sbt
@@ -6,4 +6,5 @@ scalaVersion := "2.11.7"
 
 libraryDependencies += "com.amazonaws" % "aws-java-sdk-s3" % "1.10.34"
 libraryDependencies += "org.elasticsearch" % "elasticsearch" % "2.0.0"
-libraryDependencies += "com.couchbase.client" % "java-client" % "2.2.2"
+libraryDependencies += "com.couchbase.client" % "java-client" % "2.1.5"
+libraryDependencies += "org.yaml" % "snakeyaml" % "1.16"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,18 +1,25 @@
-import java.io.{BufferedReader, InputStream, InputStreamReader}
+import java.io._
 import java.net.InetAddress
+import java.text.SimpleDateFormat
+import java.util
+import java.util.logging.Logger
 
 import com.amazonaws.auth.profile.ProfileCredentialsProvider
 import com.amazonaws.services.s3.AmazonS3Client
-import com.amazonaws.services.s3.model.{GetObjectRequest, ListObjectsRequest, ObjectListing}
+import com.amazonaws.services.s3.model.{PutObjectRequest, GetObjectRequest, ListObjectsRequest, ObjectListing}
 import com.amazonaws.util.json.{JSONException, JSONObject}
-import com.couchbase.client.java.CouchbaseCluster
-import com.couchbase.client.java.view.{ViewRow, ViewQuery}
+import com.couchbase.client.java.{Bucket, CouchbaseCluster}
+import com.couchbase.client.java.document.JsonDocument
+import com.couchbase.client.java.document.json.JsonArray
+import com.couchbase.client.java.view.ViewQuery
 import org.elasticsearch.client.transport.TransportClient
 import org.elasticsearch.common.settings.Settings
 import org.elasticsearch.common.transport.InetSocketTransportAddress
-import org.elasticsearch.common.xcontent.{XContentBuilder, XContentType, XContentFactory}
+import org.elasticsearch.common.xcontent.{XContentBuilder, XContentFactory, XContentType}
 import org.joda.time.DateTime
+import org.yaml.snakeyaml.Yaml
 
+import scala.collection.mutable
 import scala.util.control.Breaks._
 import scala.collection.JavaConversions._
 
@@ -20,93 +27,224 @@ import scala.collection.JavaConversions._
   * Created by DynamicScope on 2015. 11. 18..
   */
 object Main {
+
+  var appList : util.ArrayList[Integer] = new util.ArrayList[Integer]()
+  var couchbaseCluster : CouchbaseCluster = null
+  var couchbaseNodes : util.List[String] = new util.ArrayList[String]
+  var couchbaseBucket : Bucket = null
+  var couchbaseBucketName = ""
+  var couchbaseBucketPassword = ""
+
   def main (args: Array[String]): Unit = {
-   couchbase()
+    loadConfig()
+
+    println(couchbaseBucketName)
+    println(couchbaseNodes)
+    println(couchbaseBucketPassword)
+
+    openCouchbase()
+    couchbaseToS3()
+    closeCouchbase()
   }
 
-  def couchbase() : Unit = {
-    val cluster = CouchbaseCluster.create("172.31.15.189", "172.31.15.190", "172.31.15.191")
-    val bucket = cluster.openBucket("userhabit", "6etx[9nHC^874n")
+  def openCouchbase() : Unit = {
+    couchbaseCluster = CouchbaseCluster.create(couchbaseNodes)
+    couchbaseBucket = couchbaseCluster.openBucket(couchbaseBucketName, couchbaseBucketPassword)
+  }
 
-    val result = bucket.query(ViewQuery.from("admin", "daily_session_count").limit(10))
+  def closeCouchbase() : Unit = {
+    couchbaseCluster.disconnect()
+  }
 
-    for (row : ViewRow <- result) {
-      val doc = row.document()
+  def loadConfig() : Unit = {
+    val yaml = new Yaml()
+    val file = new File("./config.yml")
+    try {
+      val input = new FileInputStream(file)
+      val data = yaml.load(input).asInstanceOf[util.LinkedHashMap[String, Object]]
+      appList = data.getOrDefault("app_ids", new util.ArrayList[Integer]()).asInstanceOf[util.ArrayList[Integer]]
 
-      println(doc.id())
+      val couchbase = data.get("couchbase").asInstanceOf[util.LinkedHashMap[String, Object]]
+      if (couchbase != null) {
+        couchbaseNodes = couchbase.get("nodes").asInstanceOf[util.List[String]]
+        val bucket = couchbase.get("bucket").asInstanceOf[util.LinkedHashMap[String, Object]]
+        couchbaseBucketName = bucket.get("name").asInstanceOf[String]
+        couchbaseBucketPassword = bucket.get("password").asInstanceOf[String]
+      }
+    } catch {
+      case e: FileNotFoundException => println(s"${file.getCanonicalPath} : was not found.")
+    }
+  }
+
+  def getAppListFromCouchbase : util.ArrayList[Integer] = {
+    val appList = couchbaseBucket.query(ViewQuery.from("admin", "daily_session_count").groupLevel(1).reduce(true))
+    val appIds = new util.ArrayList[Integer]()
+    if (appList.success()) {
+      appList.foreach(appListRow => {
+        val appId = appListRow.key().asInstanceOf[JsonArray].get(0).toString
+        appIds.add(appId.toInt)
+      })
+    }
+    appIds
+  }
+
+  def couchbaseToS3() : Unit = {
+    var fromDateTime = new DateTime(2015, 1, 1, 0, 0)
+    val toDateTime = new DateTime(2015, 10, 18, 0, 0)
+
+    val s3Client = new AmazonS3Client(new ProfileCredentialsProvider())
+
+    if (appList.isEmpty) {
+      appList = getAppListFromCouchbase
     }
 
-    cluster.disconnect()
-  }
+    appList.foreach(intAppId => {
 
-  def elasticSearch() : Unit = {
-    val s3Client = new AmazonS3Client(new ProfileCredentialsProvider())
-    val listObjectsRequest = new ListObjectsRequest().withBucketName("userhabit-jake-test")
-    var objectListing : ObjectListing = new ObjectListing()
+      println(s"----------${intAppId.toString}----------")
 
-    val settings = Settings.settingsBuilder().build()
+      val fromDateResponse = couchbaseBucket.query(ViewQuery.from("admin", "daily_session_count")
+        .startKey(JsonArray.fromJson(s"[$intAppId]"))
+        .endKey(JsonArray.fromJson(s"[${intAppId+1}]"))
+        .reduce(false))
+      val fromDateResponseItrRows = fromDateResponse.rows()
 
-    val client = TransportClient.builder().settings(settings).build()
-      .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("172.31.1.101"), 9300))
+      if (fromDateResponse.success()) {
+        if (fromDateResponseItrRows.hasNext) {
+          val fromDateViewRow = fromDateResponseItrRows.next()
+          if (fromDateViewRow != null) {
+            val fromDateMillis = fromDateViewRow.key().asInstanceOf[JsonArray].get(1).toString.toLong
+            fromDateTime = new DateTime(fromDateMillis).withHourOfDay(0).withMinuteOfHour(0)
+          }
 
-    do {
-      objectListing = s3Client.listObjects(listObjectsRequest)
-      val objectSummaries = objectListing.getObjectSummaries
-      val newLine = "\n"
-      println(objectSummaries.size())
-      for (i <- 0 to objectSummaries.size() - 1) {
-        val key = objectSummaries.get(i).getKey
-        val s3Object = s3Client.getObject(new GetObjectRequest("userhabit-jake-test", key))
+          var eDate = toDateTime
 
-        val reader = new BufferedReader(new InputStreamReader(s3Object.getObjectContent))
-        var byteOffset = 0
-        var sessionJson = reader.readLine()
-        while (sessionJson != null) {
-          try {
-            // displayTextInputStream(s3Object.getObjectContent)
-            // val strJson = scala.io.Source.fromInputStream(s3Object.getObjectContent).mkString
-            val json = new JSONObject(sessionJson)
-            json.remove("appViewActivity")
-            json.remove("location")
-            json.remove("phoneNumber")
-            // json.remove("viewFlow")
+          while (eDate.getMillis > fromDateTime.getMillis) {
+            val sDate = eDate.minusHours(1)
 
-            val appId = json.getInt("appId")
-            val localTime = json.getLong("localTime")
-            val timestamp = new DateTime(localTime)
+            val startKey = JsonArray.fromJson(s"[$intAppId,${sDate.getMillis}]")
+            val endKey = JsonArray.fromJson(s"[$intAppId,${eDate.getMillis}]")
 
-            val indexName = s"uh-${appId}-${timestamp.toString("yyyyMMdd")}"
-            val indexType = "session"
+            val totalSessionsResult = couchbaseBucket.query(ViewQuery.from("admin", "daily_session_count").startKey(startKey).endKey(endKey).reduce(true))
+            if (totalSessionsResult.success()) {
+              var totalSessions = 0
+              totalSessionsResult.foreach(r => {
+                totalSessions = r.value().toString.toInt
+              })
 
-            val file = new JSONObject()
-            file.put("name", key)
-            file.put("offset", byteOffset)
-            json.put("file", file)
+              if (totalSessions > 0) {
+                val limit = 500
+                var skip = 0
+                val year = sDate.toString("yyyy")
+                val month = sDate.toString("MM")
+                val day = sDate.toString("dd")
+                val hour = sDate.toString("HH")
 
-            byteOffset = sessionJson.getBytes().length + newLine.getBytes().length
+                val dir = new File(s"./tmp/$intAppId/$year/$month/$day")
+                if (!dir.exists && !dir.isDirectory) {
+                  dir.mkdirs()
+                }
 
-            val isIndexExists = client.admin().indices().prepareExists(indexName).execute().actionGet().isExists
-            if (!isIndexExists) {
-              val res = client.admin().indices().prepareCreate(indexName).addMapping(indexType, getMapping(indexType)).execute().actionGet()
-              println(res.toString)
+                val file = new File(s"${dir.getCanonicalPath}/$hour")
+                val bw = new BufferedWriter(new FileWriter(file))
+
+                while (skip < totalSessions) {
+                  val result = couchbaseBucket.query(ViewQuery.from("admin", "daily_session_count").startKey(startKey).endKey(endKey).reduce(false).skip(skip).limit(limit))
+                  if (result.success()) {
+                    result.foreach(row => {
+                      val doc = row.document().content()
+                      bw.write(doc.toString)
+                      bw.newLine()
+                    })
+                  }
+                  skip += limit
+                }
+
+                bw.close()
+
+                val key = s"$intAppId/$year/$month/$day/$hour"
+                val putResult = s3Client.putObject(new PutObjectRequest("userhabit-jake-test", key, file))
+                println(putResult.getMetadata)
+
+                // Remove file when S3 upload succeeds
+                file.delete()
+              }
             }
 
-            val response = client.prepareIndex(indexName, indexType)
-              .setSource(json.toString)
-              .get()
-            //println(response)
-          } catch {
-            case e: JSONException => println(e.getMessage)
+            eDate = sDate
           }
-          sessionJson = reader.readLine()
         }
       }
-
-      listObjectsRequest.setMarker(objectListing.getNextMarker)
-    } while (objectListing.isTruncated)
-
-    client.close()
+    })
   }
+
+  //  def elasticSearch() : Unit = {
+  //    val s3Client = new AmazonS3Client(new ProfileCredentialsProvider())
+  //    val listObjectsRequest = new ListObjectsRequest().withBucketName("userhabit-jake-test")
+  //    var objectListing : ObjectListing = new ObjectListing()
+  //
+  //    val settings = Settings.settingsBuilder().build()
+  //
+  //    val client = TransportClient.builder().settings(settings).build()
+  //      .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("172.31.1.101"), 9300))
+  //
+  //    do {
+  //      objectListing = s3Client.listObjects(listObjectsRequest)
+  //      val objectSummaries = objectListing.getObjectSummaries
+  //      val newLine = "\n"
+  //      println(objectSummaries.size())
+  //      for (i <- 0 to objectSummaries.size() - 1) {
+  //        val key = objectSummaries.get(i).getKey
+  //        val s3Object = s3Client.getObject(new GetObjectRequest("userhabit-jake-test", key))
+  //
+  //        val reader = new BufferedReader(new InputStreamReader(s3Object.getObjectContent))
+  //        var byteOffset = 0
+  //        var sessionJson = reader.readLine()
+  //        while (sessionJson != null) {
+  //          try {
+  //            // displayTextInputStream(s3Object.getObjectContent)
+  //            // val strJson = scala.io.Source.fromInputStream(s3Object.getObjectContent).mkString
+  //            val json = new JSONObject(sessionJson)
+  //            json.remove("appViewActivity")
+  //            json.remove("location")
+  //            json.remove("phoneNumber")
+  //            // json.remove("viewFlow")
+  //
+  //            val appId = json.getInt("appId")
+  //            val localTime = json.getLong("localTime")
+  //            val timestamp = new DateTime(localTime)
+  //
+  //            val indexName = s"uh-${appId}-${timestamp.toString("yyyyMMdd")}"
+  //            val indexType = "session"
+  //
+  //            val file = new JSONObject()
+  //            file.put("name", key)
+  //            file.put("offset", byteOffset)
+  //            json.put("file", file)
+  //
+  //            byteOffset = sessionJson.getBytes().length + newLine.getBytes().length
+  //
+  //            val isIndexExists = client.admin().indices().prepareExists(indexName).execute().actionGet().isExists
+  //            if (!isIndexExists) {
+  //              val res = client.admin().indices().prepareCreate(indexName).addMapping(indexType, getMapping(indexType)).execute().actionGet()
+  //              println(res.toString)
+  //            }
+  //
+  //            val response = client.prepareIndex(indexName, indexType)
+  //              .setSource(json.toString)
+  //              .get()
+  //            //println(response)
+  //          } catch {
+  //            case e: JSONException => println(e.getMessage)
+  //          }
+  //          sessionJson = reader.readLine()
+  //        }
+  //      }
+  //
+  //      listObjectsRequest.setMarker(objectListing.getNextMarker)
+  //    } while (objectListing.isTruncated)
+  //
+  //    client.close()
+  //  }
 
   def getMapping(indexType : String) : XContentBuilder = {
     XContentFactory.contentBuilder(XContentType.JSON)
@@ -249,11 +387,11 @@ object Main {
       .startObject("file")
       .startObject("properties")
       .startObject("name")
-        .field("type", "string")
-        .field("index", "not_analyzed")
+      .field("type", "string")
+      .field("index", "not_analyzed")
       .endObject()
       .startObject("offset")
-        .field("type", "long")
+      .field("type", "long")
       .endObject()
       .endObject()
       .endObject()
